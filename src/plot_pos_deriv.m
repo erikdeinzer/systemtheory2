f2cca4110adecc263b242f63c3446cac0dc67529
Erik Deinzer <erik.deinzer@mailbox.org>
2022-12-07 12:42:04 +0100

    Commented Function

--- src/plot_pos_deriv.m
+++ src/plot_pos_deriv.m
@@ -1,3 +1,9 @@
+% Plots the position signal and its derivative
+% Inputs:
+%   pos: position signal (usually out.pos)
+%   deriv: derivative of position signal (usually out.deriv)
+%   norm_deriv_y: maximum y value on axis of derivative plot
+%   name: "plus" or "minus" torque value -> e.g. "plus20" or "minus5"
 function p = plot_pos_deriv(pos, deriv, norm_deriv_y, name)
     p = tiledlayout(2,1)
 
@@ -8,6 +14,7 @@ function p = plot_pos_deriv(pos, deriv, norm_deriv_y, name)
     ylabel('Position [°]') 
     yticks([0 45 90 135 180 225 270 315 360])
     axis([0 10 0 360])
+    grid on
 
     %Plot Derivate
     nexttile
@@ -15,6 +22,7 @@ function p = plot_pos_deriv(pos, deriv, norm_deriv_y, name)
     title('Derivative of Position')
     ylabel('')
     axis([0 10 -norm_deriv_y norm_deriv_y])
+    grid on
 
     exportgraphics(p, join(['../plots/','ex_202_', name, '.jpg']))
 end
