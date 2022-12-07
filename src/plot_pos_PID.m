c4ee2820869b0fddacdb067defc8a27ce13f0cf2
traskob <t-raskob@web.de>
2022-12-07 17:25:36 +0100

    PlotsEinschl3.2

---
+++ src/plot_pos_PID.m
@@ -0,0 +1,15 @@
+function p = plot_pos_PID(pos, pvalue, grad)
+    p = tiledlayout(1,1);
+
+    %Plot Position
+    nexttile
+    plot(pos)
+    yline(grad, "-", 'Führungsgröße')
+    str = join(['PID-Regler: ', pvalue, '    Führungsgröße: ', string(grad), '°']);
+    title(str)
+    ylabel('Position [°]') 
+    yticks([0 45 90 135 180 225 270 315 360])
+    axis([0 5 0 360])
+    grid on
+    exportgraphics(p, join(['../plots/Aufgabe3/','ex_301_PID_Regler_', pvalue, '_', string(grad), '.jpg']))
+end
---
+++ src/plot_pos_only.asv
@@ -0,0 +1,16 @@
+function p = plot_pos_only(pos, pvalue, grad)
+    p = tiledlayout(1,1);
+
+    %Plot Position
+    nexttile
+    plot(pos)
+    yline(grad, "-", 'Führungsgröße')
+    str = join(['P-Regler = ', pvalue, '    Führungsgröße: ', string(grad), '°']);
+    title(str)
+    ylabel('Position [°]') 
+    yticks([0 45 90 135 180 225 270 315 360])
+    axis([0 5 0 360])
+    grid on
+    exportgraphics(p, join(['../plots/Aufgabe3/','ex_301_P_Regler_', pvalue, '_', string(grad), '.jpg']))
+end
+
